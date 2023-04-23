61f9f137972b6bd4ba88e2256e3601b705a490a8
Polina <poly.gera777@gmail.com>
2023-04-23 17:27:14 +0300

    Добавление биндингов

--- app/src/main/java/com/example/tamagotchi_pet/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi_pet/MainActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tamagotchi_pet.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, StartFragment.class, null)
+                    .commit();
+        }
 
     }
 }
